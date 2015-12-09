65dc3e541a496574d0322f9c23a7cd9f32d77980
clownis <jxzwfz@qq.com>
2015-12-09 12:53:56 +0800

    finished

--- scripts/skills.js
+++ scripts/skills.js
@@ -98,7 +98,7 @@ function prepareSlideshow() {
     var item_3 = document.createElement("li");
     var item_1_text = document.createTextNode("熟练掌握HTML语言，了解HTML发展历史");
     var item_2_text = document.createTextNode("能够书写符合W3C标准的XHTML页面和语义化的代码");
-    var item_3_text = document.createTextNode("了解HTML5新标签语义并对canvas画图有简单学习和实践");
+    var item_3_text = document.createTextNode("了解HTML5新标签语义并对canvas有简单学习和实践");
 
     item_1.appendChild(item_1_text);
     item_2.appendChild(item_2_text);
@@ -114,51 +114,51 @@ function prepareSlideshow() {
   	var lists = nav.getElementsByTagName("li");
 
   	for (var i = 0; i < lists.length; i++) {
-      lists[i].onmouseover = function() {
-        var listvalue = this.innerHTML;
- 			  if (listvalue == "HTML") {
-      			moveElement("preview", 0, 0, 5); 
-            item_1.innerHTML = "熟练掌握HTML语言，了解HTML发展历史";
-            item_2.innerHTML = "能够书写符合W3C标准的XHTML页面和语义化的代码";
-            item_3.innerHTML = "了解HTML5新标签语义并对canvas画图有简单学习和实践";
- 			}
+        lists[i].onclick = function() {
+            var listvalue = this.innerHTML;
+ 			      if (listvalue == "HTML") {
+      			   moveElement("preview", 0, 0, 5); 
+                item_1.innerHTML = "熟练掌握HTML语言，了解HTML发展历史";
+                item_2.innerHTML = "能够书写符合W3C标准的XHTML页面和语义化的代码";
+                item_3.innerHTML = "了解HTML5新标签语义并对canvas有简单学习和实践";
+ 			      }
  			
-  		  if (listvalue == "CSS") {
-      			moveElement("preview", -150, 0, 5);
-            item_1.innerHTML = "熟悉DIV+CSS布局和盒子模型";
-            item_2.innerHTML = "能够运用CSS选择器和伪类技术编写样式文件";
-            item_3.innerHTML = "对CSS3新增属性和设计效果有简单了解";
-  		}
+      		  if (listvalue == "CSS") {
+          			moveElement("preview", -150, 0, 5);
+                item_1.innerHTML = "熟悉DIV+CSS布局和盒子模型";
+                item_2.innerHTML = "能够运用CSS选择器和伪类技术编写样式文件";
+                item_3.innerHTML = "对CSS3新增属性和设计效果有简单了解";
+      		  }
 	
-  		if (listvalue == "AngularJS") {
-  			moveElement("preview", -300, 0, 5);
-        item_1.innerHTML = "掌握AngularJS指令、表达式、控制器、过滤器等基本语法规则";
-        item_2.innerHTML = "理解MVC设计模式、数据双向绑定、依赖注入等基础概念";
-        item_3.innerHTML = "熟悉AngularJS框架构建CRUD应用，有过简单的实际小项目";
-  		}
+        		if (listvalue == "AngularJS") {
+        			moveElement("preview", -300, 0, 5);
+              item_1.innerHTML = "掌握AngularJS指令、表达式、控制器、过滤器等基本语法规则";
+              item_2.innerHTML = "理解MVC设计模式、数据双向绑定、依赖注入等基础概念";
+              item_3.innerHTML = "熟悉AngularJS框架构建CRUD应用，有过简单的实际小项目";
+        		}
 
-  		if (listvalue == "Bootstrap") {
-				moveElement("preview", -450, 0, 5);
-        item_1.innerHTML = "掌握网格布局及容器布局系统";
-        item_2.innerHTML = "熟悉Bootstrap前端框架内置布局组件及插件的应用";
-        item_3.innerHTML = "理解Bootstrap对于响应式网站设计的意义";
-  		}
+        		if (listvalue == "Bootstrap") {
+      				moveElement("preview", -450, 0, 5);
+              item_1.innerHTML = "掌握网格布局及容器布局系统";
+              item_2.innerHTML = "熟悉Bootstrap前端框架内置布局组件及插件的应用";
+              item_3.innerHTML = "理解Bootstrap对于响应式网站设计的意义";
+        		}
 
-  		if (listvalue == "JavaScript") {
-  			moveElement("preview", -600, 0, 5);
-        item_1.innerHTML = "掌握JavaScript语言，能够运用DOM、AJAX技术进行脚本编写";
-        item_2.innerHTML = "熟练使用浏览器开发者工具对代码进行调试、纠错和优化";
-        item_3.innerHTML = "理解并掌握模块化、面向对象的设计思想";
-  		}
+        		if (listvalue == "JavaScript") {
+        			moveElement("preview", -600, 0, 5);
+              item_1.innerHTML = "掌握JavaScript语言，能够运用DOM、AJAX技术进行脚本编写";
+              item_2.innerHTML = "熟练使用浏览器开发者工具对代码进行调试、纠错和优化";
+              item_3.innerHTML = "理解并掌握模块化、面向对象的设计思想";
+        		}
 
-  		if(listvalue == "jQuery") {
-  			moveElement("preview", -750, 0, 5);
-        item_1.innerHTML = "熟悉jQuery基本语法规则";
-        item_2.innerHTML = "了解jQuery中事件触发与DOM操作在原生JS中的相应实现";
-        item_3.innerHTML = "能够用jQuery库结合相关技术进行开发";
-  		}
-}
-  }
+        		if(listvalue == "jQuery") {
+        			moveElement("preview", -750, 0, 5);
+              item_1.innerHTML = "熟悉jQuery基本语法规则";
+              item_2.innerHTML = "了解jQuery中事件触发与DOM操作在原生JS中的相应实现";
+              item_3.innerHTML = "能够用jQuery库结合相关技术进行开发";
+        		}
+        }
+    }
 }
 
 addLoadEvent(prepareSlideshow);	
